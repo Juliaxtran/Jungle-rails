48ccf611ebed8eed7c2e8d335b02885ffce5eb97
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-10 14:45:02 -0400

    Home Page cypress test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -5,4 +5,12 @@ describe('visits our jungle page ', () => {
     cy.visit('http://localhost:3000/')
   })
 
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  })
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+
 })
