e022101d68f6baa63e26843fe071a7bc8ea61a4f
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-10 15:22:47 -0400

    Add product detail cypress test

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -4,16 +4,10 @@ describe('visit product page ', () => {
     cy.visit('http://localhost:3000/')
   })
 
-  it ('click on item , cart should contain 1 item ', () => {
-    cy.get(':nth-child(2) > div > .button_to > .btn').click({force: true})
-    cy.get(':nth-child(6) > .nav-link').contains(1)
-  })
-
-  it ('click on cart , redirects you to cart page ', () => {
-    cy.get(':nth-child(2) > div > .button_to > .btn').click({force: true})
-    cy.get(':nth-child(6) > .nav-link').contains(1)
-    cy.get(':nth-child(6) > .nav-link').click()
-    cy.visit('http://localhost:3000/cart')
+  it ('click on item , redirects you to item page, should contain a class called product detail  ', () => {
+    cy.get(':nth-child(1) > a > img').click()
+    cy.contains('article', 'Cliff Collard')
+    cy.get('.product-detail').should('exist')
   })
 
 
